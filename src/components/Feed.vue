c0b4dce9a82e048816362ee9ce784eb0714ee7de
unknown <TheTornadoTitan@gmail.com>
2019-09-21 13:03:50 -0500

    first commit

---
+++ src/components/Feed.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="feed">
+    <h1>Work Feed</h1>
+    <div>work</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'feed',
+  data: () => ({
+    feedItems: {},
+  }),
+  created() {
+    fetch('http://127.0.0.1:3000/')
+      .then(response => response.json())
+      .then(data => Object.assign(this.feedItems, data));
+    console.log(this.feedItems);
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+#feed {
+  text-align: center;
+  div {
+  }
+}
+</style>
